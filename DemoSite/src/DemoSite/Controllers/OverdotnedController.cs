973a7e19dbf0680b6283df0eabbfcc322082c797
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-02-13 08:49:31 +0100

    More pages

---
+++ DemoSite/src/DemoSite/Controllers/OverdotnedController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace DemoSite.Controllers
+{
+    public class OverdotnedController : Controller
+    {
+        private readonly IRuntimeEnvironment _runtimeEnvironment;
+
+        public OverdotnedController(IRuntimeEnvironment runtimeEnvironment)
+        {
+            _runtimeEnvironment = runtimeEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["OperatingSystem"] = _runtimeEnvironment.OperatingSystem;
+            ViewData["OperatingSystemVersion"] = _runtimeEnvironment.OperatingSystemVersion;
+            ViewData["RuntimeType"] = _runtimeEnvironment.RuntimeType;
+            ViewData["RuntimeArchitecture"] = _runtimeEnvironment.RuntimeArchitecture;
+            ViewData["RuntimeVersion"] = _runtimeEnvironment.RuntimeVersion;
+
+            return View();
+        }
+    }
+}
---
+++ DemoSite/src/DemoSite/Controllers/StudiegroepController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace DemoSite.Controllers
+{
+    public class StudiegroepController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    public class NieuwsbriefController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
